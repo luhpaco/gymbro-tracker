a2f63e12d5003d103df5501f3e445784c21ca115
Luis Paz <lpaz0073@gmail.com>
2026-08-25 16:24:31 -0500

    fix(tailwind): load config with esm imports

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,5 +1,8 @@
 import type { Config } from "tailwindcss";
-const { fontFamily } = require("tailwindcss/defaultTheme");
+import defaultTheme from "tailwindcss/defaultTheme";
+import tailwindcssAnimate from "tailwindcss-animate";
+
+const { fontFamily } = defaultTheme;
 
 const config = {
 	darkMode: ["class"],
@@ -80,7 +83,7 @@ const config = {
 			},
 		},
 	},
-	plugins: [require("tailwindcss-animate")],
+	plugins: [tailwindcssAnimate],
 } satisfies Config;
 
 export default config;
